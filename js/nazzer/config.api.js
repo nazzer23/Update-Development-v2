bf8894a74c3543a781a35f800b406841a80894d1
Ben Vernazza <nazzer23@hotmail.co.uk>
2020-01-09 11:06:57 +0000

    Changed WS Port

--- js/nazzer/config.api.js
+++ js/nazzer/config.api.js
@@ -1,5 +1,5 @@
 export let apiUrl = "http://dev-api.updatesocial.co.uk/";
-export let webSocketUrl = "ws://updatesocial.co.uk:8080";
+export let webSocketUrl = "wss://updatesocial.co.uk:8080";
 
 export let sessionCheck = "user/session/checkSession.php";
 export let loginUrl = "user/login.php";
